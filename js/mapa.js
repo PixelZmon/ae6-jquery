46cb8e6dd92a0ca4b2eaf8f51990a304c89dafea
Sebastian <sebathx@gmail.com>
2024-03-17 18:15:14 -0300

    agregado jquery

---
+++ js/mapa.js
@@ -0,0 +1,8 @@
+document.getElementById("various").addEventListener("click", () => {
+    Fancybox.show([
+      {
+        src: "https://www.google.com/maps/search/?api=1&query=-36.82665111228271%2C-73.04962795145843"
+ 
+      }
+    ]);
+  });  
---
+++ js/tabla.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('#infoTable').DataTable({
+      "language": {
+        "url": "//cdn.datatables.net/plug-ins/1.10.21/i18n/Spanish.json"
+      }
+    })
+  })
